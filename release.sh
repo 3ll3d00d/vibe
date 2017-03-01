151147a1dc213ce653bb6293c79b12c27832078f
3ll3d00d <mattkhan@gmail.com>
2017-03-01 08:12:57 +0000

    fix venv detection

--- release.sh
+++ release.sh
@@ -33,7 +33,7 @@ function init_release() {
     echo "Activating venv ${1}"
     source ${1}/bin/activate
     local INVENV=$(python -c 'import sys; print ("1" if sys.prefix != sys.base_prefix else "0")')
-    [[ ${INVENV} =  = 1 ]] || fail_hard "Did not activate venv ${1}"
+    [[ ${INVENV} == 1 ]] || fail_hard "Did not activate venv ${1}"
     echo "Upgrading setuptools "
     pip install setuptools --upgrade
     echo "Upgrading wheel "
