16d4bdb1ba8d197a72cf8fb360a77f34dcd444d8
3ll3d00d <mattkhan@gmail.com>
2017-03-21 22:03:18 +0000

    hooked up chart controller y axis range to the chart config

--- vibe-ui/src/scenes/analyse/Chart.js
+++ vibe-ui/src/scenes/analyse/Chart.js
@@ -31,36 +31,59 @@ export default class Chartjs extends PureComponent {
             };
         });
         const xLinLog = this.props.config.xLog ? "logarithmic" : "linear";
-        // TODO use this somewhere
-        const minY = 5 * Math.floor(this.props.config.y[0]/5);
-        const maxY = 5 * Math.ceil(this.props.config.y[1]/5);
-        const yTicks = [...new Array((maxY - minY)/5).keys()].map(i => (i * 5) + minY);
+        const yRange = Math.floor(this.props.config.y[1] - this.props.config.y[0]);
+        let yStep = 5;
+        if (yRange > 60) {
+            yStep = Math.floor(yRange / 10);
+        } else if (yRange < 1) {
+            yStep = 0.1;
+        } else if (yRange < 2) {
+            yStep = 0.2;
+        } else if (yRange < 5) {
+            yStep = 0.5;
+        } else if (yRange < 10) {
+            yStep = 1;
+        } else if (yRange < 20) {
+            yStep = 2;
+        }
+        const minY = yStep * Math.floor(this.props.config.y[0] / yStep);
+        const maxY = yStep * Math.ceil(this.props.config.y[1] / yStep);
+        let yAxisTicks = {
+            min: minY,
+            max: maxY,
+            stepSize: yStep
+        };
+        const options = {
+            scales: {
+                xAxes: [{
+                    type: xLinLog,
+                    position: 'bottom',
+                    ticks: {
+                        min: Math.round(this.props.config.x[0]),
+                        max: this.props.config.x[1],
+                        callback: (value, index, values) => Math.round(value)
+                    },
+                    scaleLabel: {
+                        display: true,
+                        labelString: "Frequency (Hz)"
+                    }
+                }],
+                yAxes: [{
+                    type: 'linear',
+                    ticks: yAxisTicks
+                }]
+            },
+            legend: {
+                position: 'bottom'
+            },
+            responsive: true,
+            animation: {
+                duration: 750
+            }
+        };
         return <Line ref='chart'
                      type={'line'}
                      data={{datasets: datasets}}
-                     options={{
-                         scales: {
-                             xAxes: [{
-                                 type: xLinLog,
-                                 position: 'bottom',
-                                 ticks: {
-                                     min: Math.round(this.props.config.x[0]),
-                                     max: this.props.config.x[1],
-                                     callback: (value, index, values) => Math.round(value)
-                                 },
-                                 scaleLabel: {
-                                     display: true,
-                                     labelString: "Frequency (Hz)"
-                                 }
-                             }]
-                         },
-                         legend: {
-                             position: 'bottom'
-                         },
-                         responsive: true,
-                         animation: {
-                             duration: 750
-                         }
-                     }}/>;
+                     options={options}/>;
     }
 }
