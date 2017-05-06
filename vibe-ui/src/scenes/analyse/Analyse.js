9c3820d025d625042a3b99c64f43e614302bcf74
3ll3d00d <mattkhan@gmail.com>
2017-05-06 09:36:43 +0100

    remove use of React proptypes in favour of the prop-types lib

--- vibe-ui/src/scenes/analyse/Analyse.js
+++ vibe-ui/src/scenes/analyse/Analyse.js
@@ -1,4 +1,5 @@
-import React, {Component, PropTypes} from "react";
+import React, {Component} from "react";
+import PropTypes from "prop-types";
 import {connect, PromiseState} from "react-refetch";
 import {Panel} from "react-bootstrap";
 import AnalysisNavigator from "./AnalysisNavigator";
